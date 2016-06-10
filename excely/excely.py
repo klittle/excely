fd3837009af22712325c0b4abeb52fac22a699eb
beepscore <support@beepscore.com>
2016-06-09 21:20:04 -0700

    Revise print statements to help with debugging

--- excely/excely.py
+++ excely/excely.py
@@ -108,8 +108,9 @@ if __name__ == '__main__':
                 if ((misspelled_sheet_misspelled_word != None) \
                         and (misspelled_sheet_misspelled_word == spellings_sheet_misspelled_word) \
                         and misspelled_sheet_correct_column_is_empty):
-                    print('spellings_row: ' + str(row) + ', misspelled_word: ' + misspelled_word + ', spelled_word: ' + spelled_word)
-                    print('matched misspelled_row ' + str(misspelled_row) + ' misspelled_word ' + misspelled_word)
+                    print('spellings_row: ' + str(row))
+                    print('misspelled_row ' + str(misspelled_row))
+                    print(spellings_sheet_misspelled_word + ', ' + spelled_word)
                     _ = misspelled_sheet.cell(row=misspelled_row, column=misspelled_sheet_correct_column, value=spelled_word)
                     _ = misspelled_sheet.cell(row=misspelled_row, column=status_column, value=spelled_status)
 
