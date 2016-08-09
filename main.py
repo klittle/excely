c9ca0b1140a5f7c98bd60c2ccf11e115df8fd1bc
beepscore <support@beepscore.com>
2016-08-08 20:49:20 -0700

    In main.py spellings_file_name change path

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from excely import excely
 
 #excely.read_in_write_out()
 
-spellings_file_name = 'data/spellings.xlsx'
+spellings_file_name = 'data/input/spellings.xlsx'
 misspelled_file_name = 'data/misspelled.xlsx'
 
 excely.write_spelling_matches_to_file(spellings_file_name, misspelled_file_name)
