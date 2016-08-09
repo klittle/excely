a87bc9eccb20d9e40f55fc7bf7e33fb6e2755e3d
beepscore <support@beepscore.com>
2016-08-08 21:09:46 -0700

    In main.py commented out out_filename change path

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from excely import excely
 
 # in_filename = 'data/input/in.xlsx'
-# out_filename = 'data/out.xlsx'
+# out_filename = 'data/output/out.xlsx'
 # excely.read_in_write_out(in_filename, out_filename)
 
 spellings_file_name = 'data/input/spellings.xlsx'
