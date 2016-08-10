09f332d498eaf4404f0b18a0c506ced94d294e8c
beepscore <support@beepscore.com>
2016-08-09 21:37:38 -0700

    In excely.py add write_name_matches_to_file and write_name_symbol_to_names_out. In main.py call excely.write_name_matches_to_file. TODO: test new methods

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from excely import excely
 # out_filename = 'data/output/out.xlsx'
 # excely.read_in_write_out(in_filename, out_filename)
 
-spellings_file_name = 'data/input/spellings.xlsx'
-misspelled_file_name = 'data/output/misspelled.xlsx'
+# spellings_file_name = 'data/input/spellings.xlsx'
+# misspelled_file_name = 'data/output/misspelled.xlsx'
+# excely.write_spelling_matches_to_file(spellings_file_name, misspelled_file_name)
 
-excely.write_spelling_matches_to_file(spellings_file_name, misspelled_file_name)
+names_in = 'data/input/names_in.xlsx'
+names_out = 'data/output/names_out.xlsx'
+excely.write_name_matches_to_file(names_in, names_out)
